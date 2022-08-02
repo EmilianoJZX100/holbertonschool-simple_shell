c5c817cbeed8a6489009f970f444ffbac70b20bd
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-01 18:42:04 -0700

    check cases

--- main_functions.c
+++ main_functions.c
@@ -2,19 +2,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-/**
- * _EOF - checks for end of file
- *
- */
-void _EOF(char *argv)
-{
-	if (argv)
-	{
-		argv = NULL;
-	}
 
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "\n", 1);
-
-	exit(EXIT_SUCCESS);
-}
