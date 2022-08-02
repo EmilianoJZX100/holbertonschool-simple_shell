e765bfd392e18095878c6609c3d3c3c2881e735b
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-02 16:23:43 -0700

    tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -4,10 +4,22 @@
  *
  *
  */
-int args_len(char *argv)
+int _tokenize(char *argv)
 {
 	int i;
 	char *arr[] = 1024;
+	char *delim = " \n\t"; 
+	char *tok;
 
+	tok = strtok(argv, delim);
 
-
+	while (tok != NULL)
+	{
+		strtok = (NULL, delim);
+	}
+	while (arr[i])
+	{
+		i++;
+		strdup(arr[0], tok);
+	}
+}
